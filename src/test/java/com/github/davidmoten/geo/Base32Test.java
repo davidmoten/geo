31a65de7065504bb013bf0d51c6696a3c46a1336
Dave Moten <davidmoten@gmail.com>
2013-06-20 13:33:00 +1000

    add test for Base32

--- src/test/java/com/github/davidmoten/geo/Base32Test.java
+++ src/test/java/com/github/davidmoten/geo/Base32Test.java
@@ -1,12 +1,14 @@
 package com.github.davidmoten.geo;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class Base32Test {
 
 	@Test
 	public void testEncode() {
-
+		assertEquals("15pn7", Base32.base32(1234567));
 	}
 
 }
---
+++ src/test/java/com/github/davidmoten/geo/LatLongTest.java
@@ -0,0 +1,15 @@
+package com.github.davidmoten.geo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class LatLongTest {
+
+	@Test
+	public void testToString() {
+		assertEquals("LatLong [lat=10.0, lon=20.0]",
+				new LatLong(10, 20).toString());
+	}
+
+}
