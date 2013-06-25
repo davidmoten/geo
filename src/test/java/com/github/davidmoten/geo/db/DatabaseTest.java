b6df7ab270c9d503b16d2d722ea07fa8f042da85
Dave Moten <davidmoten@gmail.com>
2013-06-26 07:45:37 +1000

    parameterize h2 unit test

--- src/test/java/com/github/davidmoten/geo/db/DatabaseTest.java
+++ src/test/java/com/github/davidmoten/geo/db/DatabaseTest.java
@@ -12,6 +12,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
+import org.junit.Test;
 
 import com.github.davidmoten.geo.Coverage;
 import com.github.davidmoten.geo.GeoHash;
@@ -19,7 +20,7 @@ import com.google.common.collect.Lists;
 
 public class DatabaseTest {
 
-	// @Test
+	@Test
 	public void testCreateDatabase() throws IOException, SQLException {
 		File dir = new File("target/db");
 		dir.mkdir();
@@ -36,7 +37,12 @@ public class DatabaseTest {
 		PreparedStatement st = con
 				.prepareStatement("insert into report(time,lat,lon,name,geohash1,geohash2,geohash3,geohash4,geohash5,geohash6,geohash7,geohash8,geohash9,geohash10,geohash11,geohash12) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
 		long now = System.currentTimeMillis();
-		for (int i = 0; i < 1000000; i++) {
+		long n;
+		if (System.getProperty("n") != null)
+			n = Long.parseLong("n");
+		else
+			n = 1000;
+		for (int i = 0; i < n; i++) {
 			long t = now - Math.round(TimeUnit.DAYS.toMillis(1));
 			double lat = -Math.random() * 10;
 			double lon = 135 + Math.random() * 10;
@@ -54,39 +60,36 @@ public class DatabaseTest {
 		for (int i = 1; i <= 12; i++)
 			execute(con, "create index idx_geohash_" + i + " on report(geohash"
 					+ i + ")");
-		for (int n = 0; n < 1; n++) {
-			for (int length = 2; length <= 4; length++) {
-				Coverage coverage = GeoHash.coverBoundingBox(-5, 136, -6, 138,
-						length);
-				System.out.println(coverage.getHashes());
-				StringBuilder s = new StringBuilder();
-				for (String hash : coverage.getHashes()) {
-					if (s.length() > 1)
-						s.append(" or ");
-					s.append("geohash" + hash.length() + "='" + hash + "'");
-				}
-				String sql2 = "select name,lat,lon from report where time >= ? and time <?  and ("
-						+ s + ")";
-				System.out.println(sql2);
-				PreparedStatement ps = con.prepareStatement(sql2);
-				ps.setLong(1, now - Math.round(TimeUnit.DAYS.toMillis(1)));
-				ps.setLong(2, now);
-				long t = System.currentTimeMillis();
-				System.out.println("querying...");
-				ResultSet rs = ps.executeQuery();
-				List<String> names = Lists.newArrayList();
-				System.out.println((System.currentTimeMillis() - t) / 1000.0
-						+ "ms");
-				while (rs.next()) {
-					String name = rs.getString(1);
-					double lat = rs.getDouble(2);
-					double lon = rs.getDouble(3);
-					if (lat >= -6 && lat <= -5 && lon >= 136 && lon < 138)
-						names.add(name);
-				}
-				System.out.println("found=" + names.size());
-				ps.close();
+		for (int length = 2; length <= 5; length++) {
+			Coverage coverage = GeoHash.coverBoundingBox(-5, 136, -6, 138,
+					length);
+			System.out.println(coverage.getHashes());
+			StringBuilder s = new StringBuilder();
+			for (String hash : coverage.getHashes()) {
+				if (s.length() > 1)
+					s.append(" or ");
+				s.append("geohash" + hash.length() + "='" + hash + "'");
 			}
+			String sql2 = "select name,lat,lon from report where time >= ? and time <?  and ("
+					+ s + ")";
+			System.out.println(sql2);
+			PreparedStatement ps = con.prepareStatement(sql2);
+			ps.setLong(1, now - Math.round(TimeUnit.DAYS.toMillis(1)));
+			ps.setLong(2, now);
+			long t = System.currentTimeMillis();
+			System.out.println("querying...");
+			ResultSet rs = ps.executeQuery();
+			List<String> names = Lists.newArrayList();
+			while (rs.next()) {
+				String name = rs.getString(1);
+				double lat = rs.getDouble(2);
+				double lon = rs.getDouble(3);
+				if (lat >= -6 && lat <= -5 && lon >= 136 && lon < 138)
+					names.add(name);
+			}
+			System.out.println("found=" + names.size() + " in"
+					+ (System.currentTimeMillis() - t) / 1000.0 + "ms");
+			ps.close();
 		}
 		con.close();
 	}
