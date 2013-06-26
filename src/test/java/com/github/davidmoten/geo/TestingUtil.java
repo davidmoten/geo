a6cc21ea9c38fff33a2b9017173fd18bd81f79b2
Dave Moten <davidmoten@gmail.com>
2013-06-26 17:26:07 +1000

    update pom and add TestingUtil class

---
+++ src/test/java/com/github/davidmoten/geo/TestingUtil.java
@@ -0,0 +1,35 @@
+package com.github.davidmoten.geo;
+
+import static org.junit.Assert.assertTrue;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+
+public class TestingUtil {
+
+	public static <T> void callConstructorAndCheckIsPrivate(Class<T> cls) {
+		Constructor<T> constructor;
+		try {
+			constructor = cls.getDeclaredConstructor();
+		} catch (NoSuchMethodException e1) {
+			throw new RuntimeException(e1);
+		} catch (SecurityException e1) {
+			throw new RuntimeException(e1);
+		}
+		assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+		constructor.setAccessible(true);
+		try {
+			constructor.newInstance();
+		} catch (InstantiationException e) {
+			throw new RuntimeException(e);
+		} catch (IllegalAccessException e) {
+			throw new RuntimeException(e);
+		} catch (IllegalArgumentException e) {
+			throw new RuntimeException(e);
+		} catch (InvocationTargetException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
