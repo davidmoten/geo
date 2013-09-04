3a6cd36f081731a15946e564ed71ded2c0125624
Dave Moten <davidmoten@gmail.com>
2013-09-04 22:19:26 +1000

    add Geomem

---
+++ src/main/java/com/github/davidmoten/geo/mem/Geomem.java
@@ -0,0 +1,34 @@
+package com.github.davidmoten.geo.mem;
+
+import java.util.Map;
+import java.util.SortedMap;
+
+import com.github.davidmoten.geo.GeoHash;
+import com.google.common.base.Optional;
+import com.google.common.collect.Maps;
+
+public class Geomem<T> {
+
+	private final Map<String, SortedMap<Long, T>> map = Maps.newConcurrentMap();
+
+	public Geomem(Optional<Integer> maxSize) {
+
+	}
+
+	public Iterable<T> find(double topLeftLat, double topLeftLong,
+			double bottomRightLat, double bottomRightLong, long start,
+			long finish) {
+		return null;
+	}
+
+	public void add(double lat, double lon, long time, T t, long expiryTime) {
+		String hash = GeoHash.encodeHash(lat, lon);
+		for (int i = 1; i <= hash.length(); i++) {
+			String key = hash.substring(0, i);
+			if (map.get(key) == null) {
+				map.put(key, Maps.<Long, T> newTreeMap());
+			}
+			map.get(key).put(time, t);
+		}
+	}
+}
