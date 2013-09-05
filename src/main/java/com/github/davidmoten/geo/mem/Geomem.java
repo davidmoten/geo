275252c794de5ef7ad615db7068fd0fa9863fe31
Dave Moten <davidmoten@gmail.com>
2013-09-05 17:28:01 +1000

    add javadoc

--- src/main/java/com/github/davidmoten/geo/mem/Geomem.java
+++ src/main/java/com/github/davidmoten/geo/mem/Geomem.java
@@ -19,20 +19,42 @@ import com.google.common.collect.Maps;
 /**
  * Provides fast concurrent querying using in memory
  * {@link ConcurrentSkipListMap}s and geohash to store data with time and
- * position.
+ * position. Depends on guava library.
  * 
  * @author dxm
  * 
  * @param <T>
+ *            The type of the record with position and time.
+ * @param <R>
+ *            The type of the id of the record with position and time.
  */
 public class Geomem<T, R> {
 
+    /**
+     * Maps from base32 geohash (long) to a map of time in epoch ms to
+     * {@link Info}.
+     */
     private final Map<Long, SortedMap<Long, Info<T, R>>> mapByGeoHash = Maps
             .newConcurrentMap();
 
+    /**
+     * Records a mapByGeoHash as above for each id of type R.
+     */
     private final Map<R, Map<Long, SortedMap<Long, Info<T, R>>>> mapById = Maps
             .newConcurrentMap();
 
+    /**
+     * Returns as an {@link Iterable} the results of a search within the
+     * bounding box given and where start <=time < finish.
+     * 
+     * @param topLeftLat
+     * @param topLeftLong
+     * @param bottomRightLat
+     * @param bottomRightLong
+     * @param start
+     * @param finish
+     * @return
+     */
     public Iterable<Info<T, R>> find(double topLeftLat, double topLeftLong,
             double bottomRightLat, double bottomRightLong, long start,
             long finish) {
@@ -49,6 +71,21 @@ public class Geomem<T, R> {
         return it;
     }
 
+    /**
+     * Returns an {@link Iterable} of {@link Info} being those records within
+     * the bounding box, start<=time < finish and inside the geoHash withinHash.
+     * 
+     * Filters first on withinHash, then time, then bounding box.
+     * 
+     * @param topLeftLat
+     * @param topLeftLong
+     * @param bottomRightLat
+     * @param bottomRightLong
+     * @param start
+     * @param finish
+     * @param withinHash
+     * @return
+     */
     private Iterable<Info<T, R>> find(final double topLeftLat,
             final double topLeftLong, final double bottomRightLat,
             final double bottomRightLong, long start, long finish,
@@ -61,6 +98,17 @@ public class Geomem<T, R> {
                         bottomRightLong));
     }
 
+    /**
+     * Returns a {@link Predicate} that returns true if and only if a point is
+     * within the bounding box, exclusive of the top (north) and left (west)
+     * edges.
+     * 
+     * @param topLeftLat
+     * @param topLeftLong
+     * @param bottomRightLat
+     * @param bottomRightLong
+     * @return
+     */
     @VisibleForTesting
     Predicate<Info<T, R>> createRegionFilter(final double topLeftLat,
             final double topLeftLong, final double bottomRightLat,
@@ -69,13 +117,22 @@ public class Geomem<T, R> {
 
             @Override
             public boolean apply(Info<T, R> info) {
-                return info.lat() >= bottomRightLat && info.lat() <= topLeftLat
-                        && info.lon() >= topLeftLong
+                return info.lat() >= bottomRightLat && info.lat() < topLeftLat
+                        && info.lon() > topLeftLong
                         && info.lon() <= bottomRightLong;
             }
         };
     }
 
+    /**
+     * Returns the {@link Info}s where start<=time <finish and position is
+     * inside the geohash withinHash.
+     * 
+     * @param start
+     * @param finish
+     * @param withinHash
+     * @return
+     */
     private Iterable<Info<T, R>> find(long start, long finish, String withinHash) {
         long key = Base32.decodeBase32(withinHash);
         SortedMap<Long, Info<T, R>> sortedByTime = mapByGeoHash.get(key);
@@ -85,20 +142,55 @@ public class Geomem<T, R> {
             return sortedByTime.subMap(start, finish).values();
     }
 
+    /**
+     * Adds a record to the in-memory store with the given position and time. Id
+     * is same as t.
+     * 
+     * @param lat
+     * @param lon
+     * @param time
+     * @param t
+     */
     @SuppressWarnings("unchecked")
     public void add(double lat, double lon, long time, T t) {
         add(lat, lon, time, t, of((R) t));
     }
 
+    /**
+     * * Adds a record to the in-memory store with the given position and time
+     * and id.
+     * 
+     * @param lat
+     * @param lon
+     * @param time
+     * @param t
+     * @param id
+     */
     public void add(double lat, double lon, long time, T t, R id) {
         add(lat, lon, time, t, of(id));
     }
 
+    /**
+     * Adds a record to the in-memory store with the given position and time and
+     * id.
+     * 
+     * @param lat
+     * @param lon
+     * @param time
+     * @param t
+     * @param id
+     */
     public void add(double lat, double lon, long time, T t, Optional<R> id) {
         Info<T, R> info = new Info<T, R>(lat, lon, time, t, id);
         add(info);
     }
 
+    /**
+     * Adds a record to the in-memory store with the given position, time and
+     * id.
+     * 
+     * @param info
+     */
     public void add(Info<T, R> info) {
         String hash = GeoHash.encodeHash(info.lat(), info.lon());
 
@@ -106,6 +198,13 @@ public class Geomem<T, R> {
         addToMapById(mapById, info, hash);
     }
 
+    /**
+     * Adds {@link Info}to the map by id.
+     * 
+     * @param mapById
+     * @param info
+     * @param hash
+     */
     private void addToMapById(
             Map<R, Map<Long, SortedMap<Long, Info<T, R>>>> mapById,
             Info<T, R> info, String hash) {
@@ -122,6 +221,13 @@ public class Geomem<T, R> {
         }
     }
 
+    /**
+     * Adds {@link Info} to the map by geohash.
+     * 
+     * @param map
+     * @param info
+     * @param hash
+     */
     private void addToMap(Map<Long, SortedMap<Long, Info<T, R>>> map,
             Info<T, R> info, String hash) {
 
