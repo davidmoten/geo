53a854bf48ab88c9fb22ba9dbc7cc69b03518463
Dave Moten <davidmoten@gmail.com>
2014-04-05 09:24:17 +1100

    change Base32.encodeHash according to #9, upgrade site plugin to 3.3 to be compatible with latest maven

--- src/main/java/com/github/davidmoten/geo/Base32.java
+++ src/main/java/com/github/davidmoten/geo/Base32.java
@@ -36,12 +36,16 @@ public final class Base32 {
 	}
 
 	/**
-	 * Returns the base 32 encoding used by geohashing from a long value.
+	 * Returns the base 32 encoding of the given length from a {@link Long}
+	 * geohash.
 	 * 
 	 * @param i
-	 * @return
+	 *            the geohash
+	 * @param length
+	 *            the length of the returned hash
+	 * @return the string geohash
 	 */
-	public static String encodeBase32(long i) {
+	public static String encodeBase32(long i, int length) {
 		char[] buf = new char[65];
 		int charPos = 64;
 		boolean negative = (i < 0);
@@ -52,9 +56,24 @@ public final class Base32 {
 			i /= 32;
 		}
 		buf[charPos] = characters[(int) (-i)];
+		String result = padLeftWithZerosToLength(new String(buf, charPos,
+				(65 - charPos)), length);
 		if (negative)
-			buf[--charPos] = '-';
-		return new String(buf, charPos, (65 - charPos));
+			return "-" + result;
+		else
+			return result;
+	}
+
+	/**
+	 * Returns the base 32 encoding of length {@link GeoHash#MAX_HASH_LENGTH}
+	 * from a {@link Long} geohash.
+	 * 
+	 * @param i
+	 *            the geohash
+	 * @return the base32 geohash
+	 */
+	public static String encodeBase32(long i) {
+		return encodeBase32(i, GeoHash.MAX_HASH_LENGTH);
 	}
 
 	/**
@@ -93,6 +112,27 @@ public final class Base32 {
 			throw new IllegalArgumentException("not a base32 character: " + ch);
 		else
 			return result;
+	}
+
+	/**
+	 * Pad left with zeros to desired string length.
+	 * 
+	 * @param s
+	 *            string to pad
+	 * @param length
+	 * @return padded string with left zeros.
+	 */
+	// @VisibleForTesting
+	static String padLeftWithZerosToLength(String s, int length) {
+		if (s.length() < length) {
+			int count = length - s.length();
+			StringBuilder b = new StringBuilder();
+			for (int i = 0; i < count; i++)
+				b.append('0');
+			b.append(s);
+			return b.toString();
+		} else
+			return s;
 	}
 
 }
