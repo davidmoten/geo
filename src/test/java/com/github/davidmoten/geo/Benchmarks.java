52fd918cc01cb7d7c24937b200f579523b3c01ad
Dave Moten <davidmoten@gmail.com>
2015-11-04 16:36:13 +1100

    add benchmark for decodeHash

--- src/test/java/com/github/davidmoten/geo/Benchmarks.java
+++ src/test/java/com/github/davidmoten/geo/Benchmarks.java
@@ -1,7 +1,5 @@
 package com.github.davidmoten.geo;
 
-import static com.github.davidmoten.geo.GeoHash.decodeHash;
-
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
@@ -9,11 +7,16 @@ import org.openjdk.jmh.annotations.State;
 @State(Scope.Benchmark)
 public class Benchmarks {
 
-    private final LatLong centre = decodeHash("dre7");
+    private final LatLong centre = GeoHash.decodeHash("dre7");
 
     @Benchmark
     public void hashContains() {
         GeoHash.hashContains("dre7", centre.getLat(), centre.getLon());
     }
-
+    
+    @Benchmark
+    public void decodeHash() {
+        GeoHash.decodeHash("dre7");
+    }
+    
 }
