39ddef57992ca1f8da17b8c3191413fa2433a337
Benjamin Bachman <bbachman@dataminr.com>
2016-03-30 17:58:11 -0400

    shows bug in bounding box coverage set

--- src/test/java/com/github/davidmoten/geo/CoverageTest.java
+++ src/test/java/com/github/davidmoten/geo/CoverageTest.java
@@ -1,5 +1,6 @@
 package com.github.davidmoten.geo;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
@@ -27,5 +28,34 @@ public class CoverageTest {
         Coverage coverage = GeoHash.coverBoundingBox(-5, 100, -45, 170);
         assertEquals(1, coverage.getHashLength());
         assertEquals(Sets.newHashSet("q", "r"), coverage.getHashes());
+    }
+
+    /**
+     * tests a bug where this bounding box doesn't return the correct results.  (-30,174) is in the "r" hash, and so
+     * any coverBoundingBox should include "r", but this one doesn't
+     */
+    @Test 
+    public void testWideCoverageBug() {
+        double top = -1;
+        double left = -26;
+        double bottom = -2;
+        double right = 175;
+
+        Coverage coverage = com.github.davidmoten.geo.GeoHash.coverBoundingBox(top, left, bottom, right, 1);
+        assertTrue(coverage.getHashes().contains("r"));
+    }
+
+    /**
+     * this test passes, while the one above fails, but the only difference is one degree in the left edge longitude
+     */
+    @Test
+    public void testLessWideCoverageBug() {
+        double top = -1;
+        double left = -25;
+        double bottom = -2;
+        double right = 175;
+
+        Coverage coverage = com.github.davidmoten.geo.GeoHash.coverBoundingBox(top, left, bottom, right, 1);
+        assertTrue(coverage.getHashes().contains("r"));
     }
 }
