389f33848cc5a01f38c824504a96f8f2e8a2f733
David Moten <davidmoten@gmail.com>
2021-09-06 08:47:55 +1000

    site dev

--- generate-site.sh
+++ generate-site.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+PROJECT=geo
 set -e
+mvn clean install 
 mvn site
+mvn site:stage -Dsite.url=file://`pwd`/target/staging-temp
 cd ../davidmoten.github.io
 git pull
-mkdir -p geo
-cp -r ../geo/target/site/* geo/
+mkdir -p $PROJECT
+cp -r ../$PROJECT/target/staging/* $PROJECT/
 git add .
-git commit -am "update site reports"
+git commit -am "update site reports for $PROJECT"
 git push
+
