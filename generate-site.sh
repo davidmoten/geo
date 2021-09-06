e98be861b48ce940f94e9fbd428cf3705bc83267
David Moten <davidmoten@gmail.com>
2021-09-06 19:39:17 +1000

    fix site generation

--- generate-site.sh
+++ generate-site.sh
@@ -3,11 +3,11 @@ PROJECT=geo
 set -e
 mvn clean install 
 mvn site
-mvn site:stage -Dsite.url=file://`pwd`/target/staging-temp
+mvn site:stage -DstagingDirectory=`pwd`/target/staging-temp
 cd ../davidmoten.github.io
 git pull
 mkdir -p $PROJECT
-cp -r ../$PROJECT/target/staging/* $PROJECT/
+cp -r ../$PROJECT/target/staging-temp/* $PROJECT/
 git add .
 git commit -am "update site reports for $PROJECT"
 git push
