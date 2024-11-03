b5dcb1d1f2d6a918130ac165b6acce6b42c59720
Vyshak Puthusseri <vyshakputhusseri@gmail.com>
2024-11-03 12:46:00 +0530

    fix: One testcase method was missing.

--- geo/src/test/java/com/github/davidmoten/geo/TestingUtil.java
+++ geo/src/test/java/com/github/davidmoten/geo/TestingUtil.java
@@ -5,6 +5,8 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 /**
  * Utility methods for unit tests.
  * 
@@ -20,10 +22,26 @@ public class TestingUtil {
      * @param cls
      */
     public static <T> void callConstructorAndCheckIsPrivate(Class<T> cls) {
-
+        Constructor<T> constructor;
+        try {
+            constructor = cls.getDeclaredConstructor();
+        } catch (NoSuchMethodException e1) {
+            throw new RuntimeException(e1);
+        } catch (SecurityException e1) {
+            throw new RuntimeException(e1);
+        }
+        assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+        constructor.setAccessible(true);
+        try {
+            constructor.newInstance();
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
     }
-
-    private static void assertTrue(boolean aPrivate) {
-    }
-
 }
